64f4e21ad4b98a63b2af27793ccf576d17b60e04
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-05-24 07:58:43 -0700

    Brought in State

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude)
+      setLong(position.coords.longitude)
+    })
+
+    console.log('Latitude is: ', lat )
+    console.log('Longitute is: ', long )
+  }, [lat, long])
+
+
   return (
     <div className="App">
       <h1>Weather Application</h1>
